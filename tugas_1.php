2dfa37cbc474dc1a76616b3267c585bf6247d58f
Yisha Zukhrufin Angelyna <yishaangeli342@gmail.com>
2023-06-15 18:05:01 +0700

    DTS-JWD

---
+++ tugas_1.php
@@ -0,0 +1,12 @@
+<?php 
+//meghitung luas lingkaran
+
+//jari-jari lingkaran
+$r = 5;
+
+//menghitug luas lingkaran
+$luas = pi() * pow($r, 2);
+
+//menampilkan hasil
+echo "Luas Lingkarang dengan jari-jari $r adalah: $luas";
+?>
---
+++ tugas_satu.php
@@ -0,0 +1,18 @@
+<?php 
+//memeriksa bilangan
+
+//memasukkan bilangan pertama
+$bilA = 5;
+
+//masukkan bilangan kedua
+$bilB = 7;
+
+//Memeriksa apakah bilangan sama atau lebih besar
+if ($bilA == $bilB){
+    echo "Kedua bilangan sama.";
+} elseif ($bilA > $bilB){
+    echo "Bilangan pertama lebih besar.";
+} else{
+    echo "Bilangan kedua lebih besar.";
+}
+?> 
